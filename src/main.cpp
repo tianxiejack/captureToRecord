60972e6f34833cb65440c7208eda9d0d383a8222
Alex <Alex@qq.com>
2020-01-19 16:59:17 +0800

    3

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "osa_thr.h"
-
+#include "osa_sem.h"
 
 Mat gFullMat;
 Mat gLeftMat;
@@ -21,36 +21,42 @@ Mat gRightMat;
 #define LEFTCAMERA 0
 #define RIGHTCAMERA 1
 
+OSA_SemHndl semRecord;
 
 int callback(void *handle, int chId, int virchId, Mat frame)
 {
 	static unsigned int t;
-
 	static unsigned int frameIndex = 0;
+
+	static bool leftflag = false , rightflag = false;
 
 	frameIndex++;
 
-	if(frameIndex < 30)
+	if(frameIndex < 60)
 		return 0;
 
 	printf("time stamp : %u , channelId = %d \n", OSA_getCurTimeInMsec(), chId);
 
-
-
-	return 0;
 	if(chId == LEFTCAMERA)
 	{
+		leftflag = true;
 		t = OSA_getCurTimeInMsec();
 		frame.copyTo(gLeftMat);
 		printf("left copy need time : %u \n" , OSA_getCurTimeInMsec() - t);
 	}
 	else if(chId == RIGHTCAMERA)
 	{
+		rightflag = true;
 		t = OSA_getCurTimeInMsec();
 		frame.copyTo(gRightMat);
 		printf("right copy need time : %u \n" , OSA_getCurTimeInMsec() - t);
 	}
 
+	if(leftflag && rightflag)
+	{
+		printf("send sem \n");
+		OSA_semSignal(&semRecord);
+	}
 
 	return 0;
 }
@@ -60,19 +66,20 @@ int callback(void *handle, int chId, int virchId, Mat frame)
 void* recordVideo(void *)
 {
 	std::string video_name = "recordDualFrame.avi";
-	VideoWriter writer = VideoWriter(video_name, CV_FOURCC('H', '2', '6', '4'), 30, Size(1920*2,1080));
+	VideoWriter writer = VideoWriter(video_name, CV_FOURCC('H', '2', '6', '5'), 30, Size(1920*2,1080));
 
 	Mat colorframe;
 	Mat grayframe;
 	static unsigned int time;
 	while(1)
 	{
+		OSA_semWait(&semRecord, 1000);
 		//cvtColor(gFullMat, grayframe, CV_YUV2GRAY_UYVY);
-		cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
+		//cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
 		time = OSA_getCurTimeInMsec();
 		//imshow("111" , colorframe);
 		//waitKey(1);
-		writer.write(colorframe);
+		//writer.write(colorframe);
 		printf("write frame need time : %u \n" , OSA_getCurTimeInMsec() - time);
 	}
 
@@ -84,6 +91,12 @@ int main()
 {
 	struct timeval tv;
 
+
+	OSA_semCreate(&semRecord, 1, 0);
+
+	OSA_ThrHndl gRecord;
+	OSA_thrCreate(&gRecord, recordVideo, 0, 0, 0);
+
 	gFullMat = Mat(1080,1920*2,CV_8UC2);
 	gLeftMat = gFullMat(Rect(0,0,1920,1080));
 	gRightMat = gFullMat(Rect(1920,0,1920,1080));
@@ -94,8 +107,6 @@ int main()
 	cap.run(LEFTCAMERA);
 	cap.run(RIGHTCAMERA);
 
-	OSA_ThrHndl gRecord;
-	//OSA_thrCreate(&gRecord, recordVideo, 0, 0, 0);
 
 	while(1)
 	{
