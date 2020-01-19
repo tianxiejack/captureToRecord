522d1dc437a4b523c7cd5fae5b2eef15bd892577
Alex <Alex@qq.com>
2020-01-19 17:56:21 +0800

    h264可以压缩，但是偶尔很卡顿，时间不均匀，会严重丢帧

--- src/main.cpp
+++ src/main.cpp
@@ -35,26 +35,27 @@ int callback(void *handle, int chId, int virchId, Mat frame)
 	if(frameIndex < 60)
 		return 0;
 
-	printf("time stamp : %u , channelId = %d \n", OSA_getCurTimeInMsec(), chId);
+	//printf("time stamp : %u , channelId = %d \n", OSA_getCurTimeInMsec(), chId);
 
 	if(chId == LEFTCAMERA)
 	{
 		leftflag = true;
 		t = OSA_getCurTimeInMsec();
 		frame.copyTo(gLeftMat);
-		printf("left copy need time : %u \n" , OSA_getCurTimeInMsec() - t);
+		//printf("left copy need time : %u \n" , OSA_getCurTimeInMsec() - t);
 	}
 	else if(chId == RIGHTCAMERA)
 	{
 		rightflag = true;
 		t = OSA_getCurTimeInMsec();
 		frame.copyTo(gRightMat);
-		printf("right copy need time : %u \n" , OSA_getCurTimeInMsec() - t);
+		//printf("right copy need time : %u \n" , OSA_getCurTimeInMsec() - t);
 	}
 
 	if(leftflag && rightflag)
 	{
 		printf("send sem \n");
+		leftflag = rightflag = false;
 		OSA_semSignal(&semRecord);
 	}
 
@@ -66,23 +67,23 @@ int callback(void *handle, int chId, int virchId, Mat frame)
 void* recordVideo(void *)
 {
 	std::string video_name = "recordDualFrame.avi";
-	VideoWriter writer = VideoWriter(video_name, CV_FOURCC('H', '2', '6', '5'), 30, Size(1920*2,1080));
+	VideoWriter writer = VideoWriter(video_name, CV_FOURCC('X', '2', '6', '4'), 15, Size(1920*2,1080));
 
 	Mat colorframe;
 	Mat grayframe;
 	static unsigned int time;
 	while(1)
 	{
-		OSA_semWait(&semRecord, 1000);
+		OSA_semWait(&semRecord, 10*1000);
 		//cvtColor(gFullMat, grayframe, CV_YUV2GRAY_UYVY);
-		//cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
+		cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
 		time = OSA_getCurTimeInMsec();
 		//imshow("111" , colorframe);
 		//waitKey(1);
-		//writer.write(colorframe);
+		writer.write(colorframe);
 		printf("write frame need time : %u \n" , OSA_getCurTimeInMsec() - time);
 	}
-
+	writer.release();
 	return NULL;
 }
 
