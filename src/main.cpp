8da678dd6ca4f2386bbaa36e95f07dc7bac312b1
Alex <Alex@qq.com>
2020-01-19 16:29:41 +0800

    2

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,19 @@ Mat gRightMat;
 int callback(void *handle, int chId, int virchId, Mat frame)
 {
 	static unsigned int t;
+
+	static unsigned int frameIndex = 0;
+
+	frameIndex++;
+
+	if(frameIndex < 30)
+		return 0;
+
+	printf("time stamp : %u , channelId = %d \n", OSA_getCurTimeInMsec(), chId);
+
+
+
+	return 0;
 	if(chId == LEFTCAMERA)
 	{
 		t = OSA_getCurTimeInMsec();
@@ -49,11 +62,17 @@ void* recordVideo(void *)
 	std::string video_name = "recordDualFrame.avi";
 	VideoWriter writer = VideoWriter(video_name, CV_FOURCC('H', '2', '6', '4'), 30, Size(1920*2,1080));
 
+	Mat colorframe;
+	Mat grayframe;
 	static unsigned int time;
 	while(1)
 	{
+		//cvtColor(gFullMat, grayframe, CV_YUV2GRAY_UYVY);
+		cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
 		time = OSA_getCurTimeInMsec();
-		writer.write(gFullMat);
+		//imshow("111" , colorframe);
+		//waitKey(1);
+		writer.write(colorframe);
 		printf("write frame need time : %u \n" , OSA_getCurTimeInMsec() - time);
 	}
 
@@ -71,15 +90,16 @@ int main()
 
 	MultiChVideo cap;
 	cap.m_usrFunc = callback;
-
+	cap.creat();
 	cap.run(LEFTCAMERA);
 	cap.run(RIGHTCAMERA);
 
 	OSA_ThrHndl gRecord;
-	OSA_thrCreate(&gRecord, recordVideo, 0, 0, 0);
+	//OSA_thrCreate(&gRecord, recordVideo, 0, 0, 0);
 
 	while(1)
 	{
+
 
 		tv.tv_sec = 2;
 		tv.tv_usec = 0;
