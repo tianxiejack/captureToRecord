1bd659e9dfadc1b846734b1a5daefb811488548f
Alex <Alex@qq.com>
2020-02-10 14:38:40 +0800

    增加queue

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,13 @@
 #include "osa_thr.h"
 #include "osa_sem.h"
 #include "appsrc.h"
+#include "osa_buf.h"
+
+
+OSA_BufCreate fisrtQuene;
+OSA_BufHndl tskfirstQuene;
+OSA_BufCreate secondQuene;
+OSA_BufHndl tsksecondQuene;
 
 
 Mat gFullMat;
@@ -56,7 +63,8 @@ int callback(void *handle, int chId, int virchId, Mat frame)
 
 	if(leftflag && rightflag)
 	{
-		printf("send sem \n");
+		int tmp = 0;
+		printf("send sem !  queneCount = %d \n" ,OSA_bufGetBufcount(&tskfirstQuene , tmp));
 		leftflag = rightflag = false;
 		OSA_semSignal(&semRecord);
 	}
@@ -65,29 +73,63 @@ int callback(void *handle, int chId, int virchId, Mat frame)
 }
 
 
+void* colorConvert(void *)
+{
+	int bufId;
+	Mat grayframe;
+	unsigned int tt;
+	while(1)
+	{
+		OSA_semWait(&semRecord, 10*1000);
+
+		//cvtColor(gFullMat, grayframe, CV_YUV2GRAY_UYVY);
+//		Mat yyy;
+//		cvtColor(gFullMat, yyy, CV_YUV2BGR_YUYV);
+//		imshow("haha",yyy);
+//		waitKey(1);
+//		continue;
+
+		int ret = OSA_bufGetEmpty(&tskfirstQuene, &bufId, 100*1000);
+		if(ret == -1)
+			continue;
+		Mat colorframe = Mat(1080,1920*2,CV_8UC3,tskfirstQuene.bufInfo[bufId].virtAddr);
+
+		tt = OSA_getCurTimeInMsec();
+		cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
+		printf("cvtColor need time : %u \n" , OSA_getCurTimeInMsec() - tt );
+
+		OSA_bufPutFull(&tskfirstQuene, bufId);
+	}
+	return NULL;
+}
+
 
 void* recordVideo(void *)
 {
 	std::string video_name = "recordDualFrame.avi";
 	VideoWriter writer = VideoWriter(video_name, CV_FOURCC('X', '2', '6', '4'), 15, Size(1920*2,1080),1);
 
-	Mat colorframe;
-	Mat grayframe;
-	static unsigned int time;
+	int bufId;
+	unsigned int tt;
 	while(1)
 	{
-		OSA_semWait(&semRecord, 10*1000);
-		//cvtColor(gFullMat, grayframe, CV_YUV2GRAY_UYVY);
-		cvtColor(gFullMat, colorframe, CV_YUV2BGR_YUYV);
+		int ret = OSA_bufGetFull(&tskfirstQuene, &bufId, 100*1000);
+		if(ret == -1)
+			continue;
 
-		time = OSA_getCurTimeInMsec();
+		Mat colorframe = Mat(1080,1920*2,CV_8UC3,tskfirstQuene.bufInfo[bufId].virtAddr);
+
+
 		//imshow("111" , colorframe);
 		//waitKey(1);
 		//pushData(gFullMat);
-		//writer.write(colorframe);
-		printf("write frame need time : %u \n" , OSA_getCurTimeInMsec() - time);
+		tt = OSA_getCurTimeInMsec();
+		writer.write(colorframe);
+		printf("write frame need time : %u \n" , OSA_getCurTimeInMsec() - tt);
+
+		OSA_bufPutEmpty(&tskfirstQuene, bufId);
 	}
-	writer.release();
+	//writer.release();
 	return NULL;
 }
 
@@ -96,8 +138,27 @@ int main()
 {
 	struct timeval tv;
 
+	fisrtQuene.numBuf = 299;
+	for (int i = 0; i < fisrtQuene.numBuf; i++)
+	{
+		fisrtQuene.bufVirtAddr[i] = (void*)malloc(1920*2*1080*3);
+		OSA_assert(fisrtQuene.bufVirtAddr[i] != NULL);
+	}
+	OSA_bufCreate(&tskfirstQuene, &fisrtQuene);
+
+//	secondQuene.numBuf = 100;
+//	for (int i = 0; i < secondQuene.numBuf; i++)
+//	{
+//		secondQuene.bufVirtAddr[i] = (void*)malloc(1920*2*1080*3);
+//		OSA_assert(secondQuene.bufVirtAddr[i] != NULL);
+//	}
+//	OSA_bufCreate(&tsksecondQuene, &secondQuene);
+
 
 	OSA_semCreate(&semRecord, 1, 0);
+
+	OSA_ThrHndl gColorConvert;
+	OSA_thrCreate(&gColorConvert, colorConvert, 0, 0, 0);
 
 	OSA_ThrHndl gRecord;
 	OSA_thrCreate(&gRecord, recordVideo, 0, 0, 0);
@@ -106,7 +167,9 @@ int main()
 	gLeftMat = gFullMat(Rect(0,0,1920,1080));
 	gRightMat = gFullMat(Rect(1920,0,1920,1080));
 
-	gstInit();
+	//gstInit();
+
+
 
 	MultiChVideo cap;
 	cap.m_usrFunc = callback;
@@ -123,6 +186,9 @@ int main()
 		tv.tv_usec = 0;
 		select(0,0,0,0,&tv);
 	}
+
+	OSA_bufDelete(&tskfirstQuene);
+//	OSA_bufDelete(&tsksecondQuene);
 
 	return 0;
 }
